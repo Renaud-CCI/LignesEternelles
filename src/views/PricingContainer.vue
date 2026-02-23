b7b0475eea6f080bcb139a492c4184bb4074bfb6
RenaudBresson <120473987+Renaud-CCI@users.noreply.github.com>
2026-02-23 17:31:03 +0100

    Pricing OK

---
+++ src/views/PricingContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <section lang="fr" aria-labelledby="pricing-title" class="w-full flex justify-center items-center my-8 xl:my-16">
+    <div class="w-full max-w-4xl relative px-4 sm:px-32">
+
+      <h2 id="pricing-title" class="pb-4">Tarifs</h2>
+
+      <ul class="list-disc pl-6 space-y-2" aria-label="Liste des tarifs">
+        <li class="md:text-lg">Frise couvercles ou côtés : <strong>à partir de 800€</strong></li>
+        <li class="md:text-lg">Jusqu'à 4 éléments 20cm x 20cm : <strong>à partir de 800€</strong></li>
+        <li class="md:text-lg">Couvercles ou côtés : <strong>à partir de 1100€</strong></li>
+        <li class="md:text-lg">Cercueil complet : <strong>à partir de 1900€</strong></li>
+      </ul>
+
+      <p class="pt-4 md:text-lg">
+        La réalisation de vos lignes éternelles dépend du temps disponible avant la cérémonie. C'est un facteur
+        important à prendre en compte en fonction de votre souhait de personnalisation.
+      </p>
+
+      <p class="md:text-lg">
+        Possibilité d'une personnalisation sur mesure. N'hésitez pas à me contacter.
+      </p>
+
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
